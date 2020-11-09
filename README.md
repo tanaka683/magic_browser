38410d4af1581916264cce62269efcc244bf5a18
田中 宏樹 <h.tanaka.683@nitech.jp>
2020-11-09 23:52:17 +0900

    デバッグ用タップスタートのX，Yを出力

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # magic_browser
+ブラウザ上でカードマジックを行うページ
+
+参考URL 
+https://dianxnao.com/javascript%E3%81%A7%E3%82%B9%E3%83%AF%E3%82%A4%E3%83%97%E5%87%A6%E7%90%86%E3%82%92%E3%81%99%E3%82%8B/
--- swipe.js
+++ swipe.js
@@ -1,3 +1,4 @@
+var mode_val;//モード格納用　0:ランダム，1:マーク，2:数字１，3:数字２
 var no;				// 数値格納用
 var number;		// 数値表示部分のDOM取得用						
 
@@ -11,63 +12,78 @@ function setSwipe(elem) {
 	let moveX;	// スワイプ中の x座標
 	let moveY;	// スワイプ中の y座標
 	let dist = 30;	// スワイプを感知する最低距離（ピクセル単位）
-	
+
 	// タッチ開始時： xy座標を取得
-	t.addEventListener("touchstart", function(e) {
+	t.addEventListener("touchstart", function (e) {
 		e.preventDefault();
 		startX = e.touches[0].pageX;
 		startY = e.touches[0].pageY;
 	});
-	
+
 	// スワイプ中： xy座標を取得
-	t.addEventListener("touchmove", function(e) {
+	t.addEventListener("touchmove", function (e) {
 		e.preventDefault();
 		moveX = e.changedTouches[0].pageX;
 		moveY = e.changedTouches[0].pageY;
 	});
-	
+
 	// タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
-	t.addEventListener("touchend", function(e) {
-		if (startX > moveX && startX > moveX + dist) {		// 右から左にスワイプ
-			previous();
-		}
-		else if (startX < moveX && startX + dist < moveX) {	// 左から右にスワイプ
-			next();
-		}
+	t.addEventListener("touchend", function (e) {
+
+numberX.innerHTML=startX;
+numberY.innerHTML=startY;
+		// if (Math.abs(startX - moveX) >= Math.abs(startY - moveY)) {
+		// 	if (startX - moveX > dist) {		// 右から左にスワイプ
+		// 		previous();
+		// 	}
+		// 	else if (startX - moveX < -1 * dist) {	// 左から右にスワイプ
+		// 		next();
+		// 	}
+		// }
+		// else {
+		// 	if (startY - moveY > dist) {//上から下にスワイプ
+
+		// 	}
+		// 	else if (startY - moveY < -1 * dist) {//下から上にスワイプ
+
+		// 	}
+		// }
 	});
 }
 
 /*
  * 次の番号を表示
  */
-function next(){
-	no ++;
+function next() {
+	no++;
 	setNumber();
 }
 
 /*
  * 前の番号を表示
  */
-function previous(){
-	no --;
+function previous() {
+	no--;
 	setNumber();
 }
 
 /*
  * 数値を画面に表示する
  */
-function setNumber(){
-	number.innerHTML = no;
+function setNumber() {
+	numberX.innerHTML = no;
 }
 
 /*
  * 起動時の処理
  */
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 	// 数値表示部分のDOM取得
-	number = document.getElementById("number");
-	
+	numberX = document.getElementById("numberX");
+	numberY = document.getElementById("numberY");
+
 	// 数値を画面に表示
+	mode_val = 0;
 	no = 0;
 	setNumber();
 
