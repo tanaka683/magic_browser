f94e9fcd6456b684bc21143fcef444175900588e
田中 宏樹 <h.tanaka.683@nitech.jp>
2020-11-10 00:50:26 +0900

    デバッグ用タップスタートのswipe_noとcounterを出力

--- swipe.js
+++ swipe.js
@@ -1,8 +1,9 @@
 var mode_val;//モード格納用　0:ランダム，1:マーク，2:数字１，3:数字２
 var show_text;//出力するテキストリスト
 var mode_counter;
-var no;				// 数値格納用
-var number;		// 数値表示部分のDOM取得用						
+var swipe_no;				// 数値格納用
+var numberX;		// 数値表示部分のDOM取得用						
+var numberY;		// 数値表示部分のDOM取得用						
 
 /*
  * スワイプイベント設定
@@ -32,33 +33,34 @@ function setSwipe(elem) {
 
 	// タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
 	t.addEventListener("touchend", function (e) {
+		//左上をタップした回数をカウント
 		if (startX < mode_dist && moveX < mode_dist
 			&& startY < mode_dist && moveY < mode_dist) {
 			mode_counter++;
 		}
 		else { mode_counter = 0; }
-
+		//左上タップ回数がmode_C以上ならmodeチェンジ
 		if (mode_counter >= mode_C && mode_val == 0) { mode_val = 1; }
 
-		numberX.innerHTML = "X: " + startX;
-		numberY.innerHTML = "Y: " + startY;
+		// numberX.innerHTML = "X: " + startX;
+		// numberY.innerHTML = "Y: " + startY;
+		//スワイプ処理
 		if (Math.abs(startX - moveX) >= Math.abs(startY - moveY)) {
 			if (startX - moveX > dist) {		// 右から左にスワイプ
-				no = 3;
+				model_cal(3);
 			}
 			else if (startX - moveX < -1 * dist) {	// 左から右にスワイプ
-				no = 1;
+				model_cal(1);
 			}
 		}
 		else {
 			if (startY - moveY > dist) {//上から下にスワイプ
-				no = 2;
+				model_cal(2);
 			}
 			else if (startY - moveY < -1 * dist) {//下から上にスワイプ
-				no = 0;
+				model_cal(0);
 			}
 		}
-		model_cal(no);
 		setNumber();
 	});
 }
@@ -68,7 +70,7 @@ function setSwipe(elem) {
  * 数値を画面に表示する
  */
 function setNumber() {
-	numberX.innerHTML = no;
+	numberX.innerHTML = swipe_no;
 	numberY.innerHTML = mode_counter;
 }
 
@@ -83,7 +85,7 @@ window.addEventListener("load", function () {
 	// 数値を画面に表示
 	mode_val = 0;
 	mode_counter = 0;
-	no = 0;
+	swipe_no = 0;
 	setNumber();
 
 	// スワイプイベント設定
