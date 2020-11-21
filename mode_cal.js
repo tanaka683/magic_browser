c08a2c92f8741d24ac64ba31f4b708b61aa37262
SatolabNIT <satolab.nit.rescue@gmail.com>
2020-11-22 02:23:06 +0900

    とりま完成

--- mode_cal.js
+++ mode_cal.js
@@ -1,14 +1,2 @@
 
 
-function model_cal(swipe_dir){//引数はスート(マーク)
-  // if(mode_val==0){
-  //     show_text="♡1"
-  // }
-  // if(mode_val==1){
-  //     if(swipe_num==0){show_text="♠"}
-  // }
-  // show_text = SUIT[swipe_dir];// enumがうまく使えねえ
-
-  show_text = SUIT[swipe_dir];
-  if(swipe_dir==SWIPE_DIR.LEFT){show_text="いえーい"+mode_val+" "+SUIT[swipe_dir];}
-}
--- swipe.js
+++ swipe.js
@@ -1,85 +1,93 @@
-var mode_val;//モード格納用　0:ランダム，1:マーク，2:数字１，3:数字２
-var show_text;//出力するテキストリスト
-var mode_cnt;
-// var swipe_no;				// 数値格納用
-var numberX;		// 数値表示部分のDOM取得用						
-var numberY;		// 数値表示部分のDOM取得用					
-var SWIPE_DIR = {RIGHT: "0", DOWN: "1", LEFT: "2", UP: "3"};//→ ↓ ← ↑// enumがうまく使えねえ
-//var SUIT = { SPADE: 0, HEART: 1, DIA: 2, CLOVER: 3};//♤  ♡  ♢   ♧// enumがうまく使えねえ
-var SUIT = { '0': '♤' , '1':'♡' , '2': '♢', '3': '♧'};//♤  ♡  ♢   ♧
-var swipe_dir;
+var mode_val = -1;//モード格納用　0:ランダム，1:マーク，2:数字１，3:数字２
+var ans = [-1, -1, -1];// [0]:SUIT,  [1][2]: 00~11までで 0 ~ 15を表す
+var show_text = "";//出力するテキストリスト
+var tap_cnt = 0;// 連続タップ回数
+
+var numberX, numberY;		// 数値表示部分のDOM取得用					
+var SWIPE_DIR = {RIGHT: 0, DOWN: 1, LEFT: 2, UP: 3};//→ ↓ ← ↑
+var SUIT = { 0: '♤' , 1:'♡' , 2: '♢', 3: '♧'};//♤  ♡  ♢   ♧
+var swipe_dir = -1;
+
+function mode_cal(){
+	
+	if(mode_val == 1) ans[0] = swipe_dir;
+	else if(mode_val == 2) ans[1] = swipe_dir;
+	else if(mode_val == 3){
+	  ans[2] = swipe_dir;
+	  var num = ans[1] * 4 + ans[2] * 1;
+	  show_text = SUIT[ans[0]];
+	  show_text += num;
+	  numberZ.innerHTML = " ans: " + ans;
+	  numberZ.innerHTML += "  じゃああああん";
+	  return;
+	}
+	numberZ.innerHTML = " ans: " + ans;
+	show_text = SUIT[Math.floor(Math.random() * 4)];// SUIT[0~3]
+	show_text += Math.floor(Math.random() * 13) + 1;// 0~12  + 1 toStringじゃなくて良いらしい(てかtoStringだとエラー出た)
+	
+	// if(swipe_dir==SWIPE_DIR.LEFT){show_text="いえーい"+mode_val+" "+SUIT[swipe_dir];}
+  }
+  
+
 /*
  * スワイプイベント設定
  */
 function setSwipe(elem) {
 	let t = document.querySelector(elem);
-	let startX;		// タッチ開始 x座標
-	let startY;		// タッチ開始 y座標
-	let moveX;	// スワイプ中の x座標
-	let moveY;	// スワイプ中の y座標
+	let startX = -1, startY = -1, endX = -1, endY = -1;		// タッチ開始(x, y), タッチ終了(x, y);
 	const MIN_DIST = 30;	// スワイプを感知する最低距離（ピクセル単位）
-	const CHMOD_RANGE = 75;//モード切替をする範囲
-	const CHMOD_THRESH = 3;//mode_C回左上をタップするとモードチェンジ
+	// const CHMOD_RANGE = 70;//モード切替をする範囲
+	const CHMOD_THRESH = 3;//CHMOD_THRESH回左上をタップするとモードチェンジ
 	// タッチ開始時： xy座標を取得
 	t.addEventListener("touchstart", function (e) {
 		e.preventDefault();
 		startX = e.touches[0].pageX;
 		startY = e.touches[0].pageY;
-		moveX = startX;
-		moveY = startY;
+		endX = startX;
+		endY = startY;
 	});
 
 	// スワイプ中： xy座標を取得
 	t.addEventListener("touchmove", function (e) {
 		e.preventDefault();
-		moveX = e.changedTouches[0].pageX;
-		moveY = e.changedTouches[0].pageY;
+		endX = e.changedTouches[0].pageX;
+		endY = e.changedTouches[0].pageY;
 	});
 
 	// タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
 	t.addEventListener("touchend", function (e) {
-		//左上をタップした回数をカウント
-		if (startX < CHMOD_RANGE && moveX < CHMOD_RANGE
-			&& startY < CHMOD_RANGE && moveY < CHMOD_RANGE) {
-			mode_cnt++;
-		}
-		else { mode_cnt = 0; }
-		//左上タップ回数がmode_C以上ならmodeチェンジ
-		if (mode_cnt >= CHMOD_THRESH && mode_val == 0) { mode_val = 1; }
 
-		// numberX.innerHTML = "X: " + startX;
-		// numberY.innerHTML = "Y: " + startY;
-		//スワイプ処理
-		if (Math.abs(startX - moveX) >= Math.abs(startY - moveY)) {
-			if (startX - moveX > MIN_DIST) {		// 右から左にスワイプ
-				// swipe_no=3;
-				swipe_dir = SWIPE_DIR.LEFT
-				model_cal(swipe_dir);
+		// スワイプorタップ判定
+		if(Math.abs(startX - endX) < MIN_DIST){
+			if(Math.abs(startY - endY) < MIN_DIST){
+				tap_cnt++;//連続タップ回数
+				if (tap_cnt >= CHMOD_THRESH){ mode_val = 1;}
+				
+				numberY.innerHTML = " startX: " + startX.toFixed(2) + "      startY: " + startY.toFixed(2) + "</br> endX: " + endX.toFixed(2) + "         endY: " + endY.toFixed(2);
+				numberX.innerHTML = " mode_val: "+mode_val + " tap_cnt: " + tap_cnt + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " endX: " + endX + " endY: " + endY;
+				return;//タップの場合は終了
 			}
-			else if (startX - moveX < -1 * MIN_DIST) {	// 左から右にスワイプ
-				// swipe_no=1;
-				//swipe_dir = '♤';
-				 swipe_dir = SWIPE_DIR.RIGHT;
-				model_cal(swipe_dir);
-			}
+		}
+		
+		//以下スワイプの場合の処理
+		tap_cnt = 0;
+		
+		//スワイプ処理 // 左上(0, 0),  右下(INF, INF)
+		if (Math.abs(endX - startX) >= Math.abs(endY - startY)) {
+		  if (endX - startX> 0) { swipe_dir = SWIPE_DIR.RIGHT;}		// 左から右にスワイプ
+		  else if (endX - startX < 0) {swipe_dir = SWIPE_DIR.LEFT;}	// 右から左にスワイプ
 		}
 		else {
-			if (startY - moveY > MIN_DIST) {//上から下にスワイプ
-				// swipe_no=2;
-				//swipe_dir = '♡';
-				 swipe_dir = SWIPE_DIR.DOWN
-				model_cal(swipe_dir);
-			}
-			else if (startY - moveY < -1 * MIN_DIST) {//下から上にスワイプ
-				// swipe_no=0;
-				//swipe_dir = '♧';
-				 swipe_dir = SWIPE_DIR.UP
-				model_cal(swipe_dir);
-			}
+		  if (endY - startY > 0) {swipe_dir = SWIPE_DIR.DOWN;}//上から下にスワイプ
+		  else if (endY - startY < 0) {swipe_dir = SWIPE_DIR.UP}//下から上にスワイプ
 		}
-		setNumber();
-		numberY.innerHTML = " startX: " + startX.toFixed(2) + "      startY: " + startY.toFixed(2) + "</br> moveX: " + moveX.toFixed(2) + "         moveY: " + moveY.toFixed(2);
-	// numberZ.innerHTML = toString(SWIPE_DIR.RIGHT);
+
+		mode_cal();
+		if(mode_val > 0) mode_val = (mode_val+1) % 5;
+		
+		numberY.innerHTML = " startX: " + startX.toFixed(2) + "      startY: " + startY.toFixed(2) + "</br> endX: " + endX.toFixed(2) + "         endY: " + endY.toFixed(2);
+		numberX.innerHTML = " mode_val: "+mode_val + " tap_cnt: " + tap_cnt + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " endX: " + endX + " endY: " + endY;
+
 	});
 
 }
@@ -88,16 +96,7 @@ function setSwipe(elem) {
 /*
  * 数値を画面に表示する
  */
-function setNumber() {
-	// numberX.innerHTML = "swipe"+swipe_no;
-	// numberY.innerHTML = "mode"+mode_cnt;
-	// numberZ.innerHTML =  "text"+show_text;
-	numberX.innerHTML = " swipe_no: "+swipe_no + " mode_cnt: " + mode_cnt + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " moveX: " + moveX + " moveY: " + moveY;
-	numberY.innerHTML = " startX: " + "      startY: "  + "</br> moveX: " + "         moveY: ";
-
-	// swipe_dir = SWIPE_DIR.RIGHT;
-	// if(swipe_dir == SWIPE_DIR.RIGHT) numberZ.innerHTML = " aaa";//SWIPE_DIR["DOWN"];//" SUIT[0]: " + SUIT['0'];// + " SUIT[1]: " + SUIT[1] + " SUIT[swipe_dir]: " + SUIT[swipe_dir];
-	// console.log(SWIPE_DIR.RIGHT);
+function setNumber() {//numberX.innerHTML = " mode_val: "+mode_val + " tap_cnt: " + tap_cnt + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " endX: " + endX + " endY: " + endY;
 }
 
 /*
@@ -110,11 +109,8 @@ window.addEventListener("load", function () {
 	numberZ = document.getElementById("numberZ");
 
 	// 数値を画面に表示
-	mode_val = 0;
-	mode_cnt = 0;
-	swipe_no = 0;
-	show_text="♡"
-	setNumber();
+	numberX.innerHTML = " mode_val: "+mode_val + " tap_cnt: " + tap_cnt + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " endX: " + endX + " endY: " + endY;
+
 
 	// スワイプイベント設定
 	setSwipe("#contents");
