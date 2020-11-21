b1873754034623ee10d9cb3bc7c7a3354b6b8c57
SatolabNIT <satolab.nit.rescue@gmail.com>
2020-11-21 18:51:52 +0900

    add_comment

--- swipe.js
+++ swipe.js
@@ -1,6 +1,6 @@
 var mode_val;//モード格納用　0:ランダム，1:マーク，2:数字１，3:数字２
 var show_text;//出力するテキストリスト
-var mode_counter;
+var CHMOD_THRESHounter;
 var swipe_no;				// 数値格納用
 var numberX;		// 数値表示部分のDOM取得用						
 var numberY;		// 数値表示部分のDOM取得用						
@@ -14,9 +14,9 @@ function setSwipe(elem) {
 	let startY;		// タッチ開始 y座標
 	let moveX;	// スワイプ中の x座標
 	let moveY;	// スワイプ中の y座標
-	let dist = 30;	// スワイプを感知する最低距離（ピクセル単位）
-	let mode_dist = 50;//モード切替をする範囲
-	let mode_C = 3;//mode_C回左上をタップするとモードチェンジ
+	const MIN_DIST = 30;	// スワイプを感知する最低距離（ピクセル単位）
+	const CHMOD_RANGE = 50;//モード切替をする範囲
+	const CHMOD_THRESH = 3;//CHMOD_THRESH回左上をタップするとモードチェンジ
 	// タッチ開始時： xy座標を取得
 	t.addEventListener("touchstart", function (e) {
 		e.preventDefault();
@@ -34,33 +34,33 @@ function setSwipe(elem) {
 	// タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
 	t.addEventListener("touchend", function (e) {
 		//左上をタップした回数をカウント
-		if (startX < mode_dist && moveX < mode_dist
-			&& startY < mode_dist && moveY < mode_dist) {
-			mode_counter++;
+		if (startX < CHMOD_RANGE && moveX < CHMOD_RANGE
+			&& startY < CHMOD_RANGE && moveY < CHMOD_RANGE) {
+			CHMOD_THRESHounter++;
 		}
-		else { mode_counter = 0; }
-		//左上タップ回数がmode_C以上ならmodeチェンジ
-		if (mode_counter >= mode_C && mode_val == 0) { mode_val = 1; }
+		else { CHMOD_THRESHounter = 0; }
+		//左上タップ回数がCHMOD_THRESH以上ならmodeチェンジ
+		if (CHMOD_THRESHounter >= CHMOD_THRESH && mode_val == 0) { mode_val = 1; }
 
 		// numberX.innerHTML = "X: " + startX;
 		// numberY.innerHTML = "Y: " + startY;
 		//スワイプ処理
 		if (Math.abs(startX - moveX) >= Math.abs(startY - moveY)) {
-			if (startX - moveX > dist) {		// 右から左にスワイプ
+			if (startX - moveX > MIN_DIST) {		// 右から左にスワイプ
 				swipe_no=1;
 				model_cal(swipe_no);
 			}
-			else if (startX - moveX < -1 * dist) {	// 左から右にスワイプ
+			else if (startX - moveX < -1 * MIN_DIST) {	// 左から右にスワイプ
 				swipe_no=1;
 				model_cal(swipe_no);
 			}
 		}
 		else {
-			if (startY - moveY > dist) {//上から下にスワイプ
+			if (startY - moveY > MIN_DIST) {//上から下にスワイプ
 				swipe_no=2;
 				model_cal(swipe_no);
 			}
-			else if (startY - moveY < -1 * dist) {//下から上にスワイプ
+			else if (startY - moveY < -1 * MIN_DIST) {//下から上にスワイプ
 				swipe_no=0;
 				model_cal(swipe_no);
 			}
@@ -76,9 +76,9 @@ function setSwipe(elem) {
  */
 function setNumber() {
 	// numberX.innerHTML = "swipe"+swipe_no;
-	// numberY.innerHTML = "mode"+mode_counter;
+	// numberY.innerHTML = "mode"+CHMOD_THRESHounter;
 	// numberZ.innerHTML =  "text"+show_text;
-	numberX.innerHTML = " swipe_no: "+swipe_no + " mode_counter: " + mode_counter + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " moveX: " + moveX + " moveY: " + moveY;
+	numberX.innerHTML = " swipe_no: "+swipe_no + " CHMOD_THRESHounter: " + CHMOD_THRESHounter + " show_text: " + show_text ;//+ " startX: " + startX + " startY: " + startY + " moveX: " + moveX + " moveY: " + moveY;
 	
 }
 
@@ -93,7 +93,7 @@ window.addEventListener("load", function () {
 
 	// 数値を画面に表示
 	mode_val = 0;
-	mode_counter = 0;
+	CHMOD_THRESHounter = 0;
 	swipe_no = 0;
 	show_text="♡"
 	setNumber();
